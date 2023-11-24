55d804c14fb740d841793a51059990d6c96e7c24
vitmax1 <2california@mail.ru>
2023-11-24 18:16:40 +0600

    range-script-ed4

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const rangeInput = document.querySelectorAll(".range-input input"),
     priceInput = document.querySelectorAll(".price-input input"),
     range = document.querySelector(".slider .progress");
 let priceGap = 1000;
-rangeInput.forEach((input) => {
+priceInput.forEach((input) => {
     input.addEventListener("input", (e) => {
         let minPrice = parseInt(priceInput[0].value),
             maxPrice = parseInt(priceInput[1].value);
@@ -19,4 +19,21 @@ rangeInput.forEach((input) => {
         }
     });
 });
-console.log(rangeInput);
+rangeInput.forEach((input) => {
+    input.addEventListener("input", (e) => {
+        let minVal = parseInt(rangeInput[0].value),
+            maxVal = parseInt(rangeInput[1].value);
+        if (maxVal - minVal < priceGap) {
+            if (e.target.className === "range-min") {
+                rangeInput[0].value = maxVal - priceGap;
+            } else {
+                rangeInput[1].value = minVal + priceGap;
+            }
+        } else {
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+            range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+        }
+    });
+});
